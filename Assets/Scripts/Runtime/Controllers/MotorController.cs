e16b1a5f54bf6fe39d8580c6b91270dc4a582f45
Bearicade <spellshot@yahoo.ca>
2020-03-24 19:48:54 -0400

    Initial Commit

---
+++ Assets/Scripts/Runtime/Controllers/MotorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorController : MonoBehaviour
+{
+    private Rigidbody2D m_rigidbody;
+
+
+    //Note: sprite size must be taken into account
+
+    private void Start()
+    {
+        if (m_rigidbody == null)
+        {
+            m_rigidbody = GetComponent<Rigidbody2D>();
+        }
+    }
+
+
+
+    //motor given behaviour/pattern and then executes based on range/offset
+}
