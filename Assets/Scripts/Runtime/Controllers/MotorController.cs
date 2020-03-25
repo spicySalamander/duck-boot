4ee64bd8514a1dce12ce5565fb1ad4861a334cc9
Bearicade <spellshot@yahoo.ca>
2020-03-25 16:13:02 -0400

    Merged with master

--- Assets/Scripts/Runtime/Controllers/MotorController.cs
+++ Assets/Scripts/Runtime/Controllers/MotorController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class MotorController : MonoBehaviour
 {
     private Rigidbody2D m_rigidbody;
+    private Vector2 m_currentTarget;
 
+    [SerializeField] private float m_motorSpeed = 0;
 
     //Note: sprite size must be taken into account
 
@@ -15,9 +17,41 @@ public class MotorController : MonoBehaviour
         {
             m_rigidbody = GetComponent<Rigidbody2D>();
         }
+
+        if (m_motorSpeed == 0)
+        {
+            //m_motorSpeed = defaultSpeed for type of unit;
+            m_motorSpeed = 1f;  //temporary
+        }
     }
 
+    //Move towards point (raw)
+    public void MoveToPointWithRigidbody(Vector2 target)
+    {
+        if (GetComponent<Rigidbody2D>())
+        {
+            m_rigidbody.velocity = target.normalized * m_motorSpeed;
+            m_currentTarget = target;
 
+            //MOVE THIS MUST BE CALLED EVERY UPDATE
+            //the current rigidboy position - the target position yields a result less than the velocity, then stop at target?
+            if (m_rigidbody.position.magnitude - target.magnitude <= m_rigidbody.velocity.magnitude)
+            {
+                m_rigidbody.position = target;
+            }
 
-    //motor given behaviour/pattern and then executes based on range/offset
+        }
+        else
+        {
+            Debug.LogWarning("There is no rigidbody present.");
+        }
+    }
+
+    //Move towards point with reference to object, object should have float for range of interaction
+    public void MoveToTargetWithRigidbody(ICombatTargetFinding target)
+    {
+
+    }
+
+    //Move away from point -- redundant
 }
