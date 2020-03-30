170fde512a0c7c1a629285e4e075eabc3dbdc2a0
Bearicade <spellshot@yahoo.ca>
2020-03-29 22:38:16 -0400

    Main screen functional

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+
+    public float targetTime;
+    private bool collided;
+    private GameObject duck;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        duck = collision.gameObject;
+        collided = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        collided = false;
+    }
+
+    private void Update()
+    {
+        OnUpdate();
+    }
+
+
+    public void OnUpdate()
+    {
+        if (collided)
+        {
+
+            if (Input.GetMouseButtonUp(0) && duck.tag == "Duck")
+            {
+                Debug.Log("Collding while mouse is up");
+
+                float currCountdownValue;
+                StartCoroutine(Timer());
+                IEnumerator Timer(float countdownValue = 10)
+                {
+                    currCountdownValue = countdownValue;
+                    while (currCountdownValue > 0)
+                    {
+                        Debug.Log("Countdown: " + currCountdownValue);
+                        duck.GetComponent<SpriteRenderer>().enabled = false;
+                        yield return new WaitForSeconds(1.0f);
+                        currCountdownValue--;
+                    }
+
+                    duck.GetComponent<SpriteRenderer>().enabled = true;
+                }
+
+                    Debug.Log(targetTime);
+            }
+        }
+    }
+
+
+    void timerEnded()
+    {
+        
+    }
+
+
+}
---
+++ Assets/Scripts/DragnDrop.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragnDrop : MonoBehaviour
+{
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isBeingHeld == true)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            // Vector 3 to hold the X and Y value of the mouse
+            Vector3 mousePos;
+            //Getting the mouse position
+            mousePos = Input.mousePosition;
+            //Convert the screen point of the mouse to the world point (the in game point)
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            
+            //locks to mouse position instead of snapping the middle of the object to the mouse 
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isBeingHeld = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+
+        isBeingHeld = false;
+
+        //snaps to default position when mouse button is lifted
+        this.gameObject.transform.localPosition = new Vector3(0, 0, 0);
+
+    }
+}
